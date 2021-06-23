aef33988dc884e0528450fb64408ebc6f82b6b74
ladiakshith802 <82776913+ladiakshith802@users.noreply.github.com>
2021-06-23 15:07:21 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,22 @@
+capital =int(input("Type the principal = "))
+time = int(input("Type the no. of years = "))
+interestrt = int(input("Type the interest rate = "))
+intafd = (int(interestrt) / 100)
+#interestrt means interest rate
+#intafd means interest after division 
+interest = (capital * intafd * time)
+amount = (int(capital) + int(interest))
+intafdcd = (1 + intafd)
+compounded_amt  = (capital * intafdcd ** time)
+compounded_int = (int(compounded_amt) - int(capital))
+print("interest:")
+print(interest)
+print("Total amount:")
+print(amount)
+print("Compound interest:")
+print(compounded_int)
+print("Total Compounded amount")
+print(compounded_amt)
+print( )
+print("Thank you,for visiting.")
+
